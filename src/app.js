24fa13cf12b0bc2bd08b320884d0d10109af061d
Trishna Patel <trishnamorar@gmail.com>
2022-11-03 14:47:38 -0500

    Added icon

--- src/app.js
+++ src/app.js
@@ -21,21 +21,29 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
-  response.data.main.temp;
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.main.wind);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png`
+  );
 }
 
 let apiKey = "f1a03t171ee4df4ba7f964ba18abo4b3";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?tulsa&apiid=${apiKey}&units=metric`;
+let city = "Tulsa";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(url).then(displayTemperature);
